a7a6d6d38dff8c8f9db004961bf32169bcd68da6
rgstech <guimaorod@gmail.com>
2023-06-17 18:34:35 -0300

    removing class based view experiment.

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,9 @@
 
 from django.urls import path
 from . import views
-from .views import HomePageView
 
 urlpatterns = [
     
-        path('', views.home, name='home'),
-        path('teste/', HomePageView.as_view(), name='teste'),
+        path('', views.home, name='home')
+        
 ]
